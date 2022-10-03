083dd3e4d88bdbd1613510c1e09d59efa39a1632
tomek272 <thomas.stach1@gmx.de>
2022-10-03 22:07:03 +0200

    Minor improvements.

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import webbrowser
 import csv
 
-# Todo: think of how to clean code
-# Todo: think of how to simplify application (vision: one click)
-
 def main():
     # Load universal parameters
     prefix = "https://www.ebay-kleinanzeigen.de"
     postal_code = "s-21337"
-    price_min = "0"
-    # price_max = "50"
-    # object = "schreibtischstuhl"
     region_key = "k0l3262r" #This weird key depends on the region.
-    # radius = "5"
 
     # Load objects from list
     file_in = "items.csv"
@@ -22,15 +15,20 @@ def main():
 
         # Check every object on Kleinanzeigen
         for item in items_reader:
+
             # Load object parameters
             object = item[0]
             radius = item[1]
-            price_max = item[2]
+            price_min = item[2]
+            price_max = item[3]
+
+            # Todo: override item configuration by argparser
 
             # Set up and open parametrized object link
             link = (prefix + "/" + postal_code + "/" + "preis:" + price_min + ":" +
                     price_max + "/" + object + "/" + region_key + radius)
 
+            # Todo: do not open all items at once
             webbrowser.open(link)
 
 if __name__ == "__main__":
